af59c9f5e439fe27c2df57034c8883408a631f7e
SammyG <Samgriffiths002@gmail.com>
2024-02-26 09:25:44 -0800

    I created some thingies

---
+++ app/src/main/java/com/example/apiproject/SinglePlayerStatsDetails.kt
@@ -0,0 +1,11 @@
+package com.example.apiproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SinglePlayerStatsDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_player_stats_details)
+    }
+}
